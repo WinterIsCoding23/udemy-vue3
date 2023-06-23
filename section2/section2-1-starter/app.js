1f2436be2155e0e69f648e55973be4ef0288225f
Markus Winter <markusdominicwinter@hotmail.com>
2023-06-23 13:21:37 +0200

    save section2-13 multiple vue instances

--- section2/section2-1-starter/app.js
+++ section2/section2-1-starter/app.js
@@ -1,1 +1,19 @@
-Vue.createApp({}).mount("#app");
+// Instance 1:
+Vue.createApp({
+  data() {
+    return {
+      firstName: "John",
+      lastName: "Doe",
+    };
+  },
+}).mount("#app");
+
+// Instance 2:
+// Vue.createApp({
+//   data() {
+//     return {
+//       firstName: "Jane",
+//       lastName: "Doe",
+//     };
+//   },
+// }).mount("#app2");
