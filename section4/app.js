0bfa918b6a0f136bbabcf196b57cc87138d37e1e
Markus Winter <markusdominicwinter@hotmail.com>
2023-07-02 19:43:59 +0200

    add comment

--- section4/app.js
+++ section4/app.js
@@ -4,6 +4,30 @@ let vm = Vue.createApp({
       message: "Hello world!",
     };
   },
+  beforeCreate() {
+    console.log("beforeCreate() function called,", this.message);
+  },
+  created() {
+    console.log("created() function called", this.message);
+  },
+  beforeMount() {
+    console.log("beforeMount() function called", this.$el);
+  },
+  mounted() {
+    console.log("mounted() function called", this.$el);
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate() function called, ", "this.$el:", this.$el);
+  },
+  updated() {
+    console.log("updated() function called", "this.$el:", this.$el);
+  },
+  beforeUnmount() {
+    console.log("beforeUnmount() function called, ", this.$el);
+  },
+  unmounted() {
+    console.log("unmounted() function called, ", this.$el);
+  },
 });
 
 vm.mount("#app");
