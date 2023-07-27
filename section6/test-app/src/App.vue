43c4c03574086f61d26587afb3b8fe5263d1e902
Markus Winter <markusdominicwinter@hotmail.com>
2023-07-27 17:41:24 +0200

    add comments & add age-data as props to user-Component

--- section6/test-app/src/App.vue
+++ section6/test-app/src/App.vue
@@ -2,18 +2,25 @@
   <h3>Hey!</h3>
   <!-- Components can be inserted with lower-case or Upper-case -->
   <greeting></greeting>
-  <user></user>
+  <!-- if written like <user age="age"></user> </user>-> Vue would assume a String has to be passed down -->
+  <!-- ":" in front needed to bind the value inside the data-function -->
+  <user :age="age"></user>
 </template>
 
 <script>
 import Greeting from "@/Components/Greeting.vue";
-import User from "@Components/User.vue";
+import User from "@/Components/User.vue";
 
 export default {
   name: "App",
   components: {
     Greeting,
     User,
+  },
+  data() {
+    return {
+      age: 20,
+    };
   },
 };
 </script>
