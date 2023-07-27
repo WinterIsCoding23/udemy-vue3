58bb90a6e0cb41ebeccdf2c3df9f0782257cb841
Markus Winter <markusdominicwinter@hotmail.com>
2023-07-27 17:53:31 +0200

    add new p in Greeting & add if-else-statement to view msg depending on age

--- section6/test-app/src/App.vue
+++ section6/test-app/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <h3>Hey!</h3>
   <!-- Components can be inserted with lower-case or Upper-case -->
-  <greeting></greeting>
+  <greeting :age="age"></greeting>
   <!-- if written like <user age="age"></user> </user>-> Vue would assume a String has to be passed down -->
   <!-- ":" in front needed to bind the value inside the data-function -->
   <user :age="age"></user>
