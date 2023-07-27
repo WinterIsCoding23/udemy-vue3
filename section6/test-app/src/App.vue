23131682ea2b8d8b71856773f4dbd83428e5ae0b
Markus Winter <markusdominicwinter@hotmail.com>
2023-07-27 17:32:52 +0200

    render User-component inside App & change first tag from p to h3

--- section6/test-app/src/App.vue
+++ section6/test-app/src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <p>Hey!</p>
+  <h3>Hey!</h3>
+  <!-- Components can be inserted with lower-case or Upper-case -->
   <greeting></greeting>
+  <user></user>
 </template>
 
 <script>
 import Greeting from "@/Components/Greeting.vue";
+import User from "@Components/User.vue";
 
 export default {
   name: "App",
   components: {
     Greeting,
+    User,
   },
 };
 </script>
