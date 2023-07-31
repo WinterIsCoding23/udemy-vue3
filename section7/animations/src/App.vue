b6025cdbcf30deec37da54345ab4708473d76448
Markus Winter <markusdominicwinter@hotmail.com>
2023-07-31 12:38:52 +0200

    add fade-transition to h2-tag

--- section7/animations/src/App.vue
+++ section7/animations/src/App.vue
@@ -1,9 +1,28 @@
-<template></template>
+<template>
+  <button type="button" @click="flag = !flag">Toggle</button>
+
+  <transition name="fade">
+    <h2 v-if="flag">Hello World!</h2>
+  </transition>
+</template>
 
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      flag: false,
+    };
+  },
 };
 </script>
 
-<style></style>
+<style>
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 0.25s linear;
+}
+</style>
