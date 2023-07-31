34f081496dc1dc337320977e12d4700333d33e8d
Markus Winter <markusdominicwinter@hotmail.com>
2023-07-31 11:20:02 +0200

    add activated- & deactivated-life-cycle-hooks to Home

--- section7/dynamic-components/src/components/Home.vue
+++ section7/dynamic-components/src/components/Home.vue
@@ -6,5 +6,12 @@ export default {
   unmounted() {
     console.log("Home component is unmounted");
   },
+  // life-cycle-hooks activated & deactivated --> opportunity to run code when a component is activated ("mounted") and deactivated ("unmounted")
+  activated() {
+    console.log("Home component is activated");
+  },
+  deactivated() {
+    console.log("Home component is deactivated");
+  },
 };
 </script>
