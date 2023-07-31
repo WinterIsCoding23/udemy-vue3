fe67b92e52317d904f258cbedf449fec5238548b
Markus Winter <markusdominicwinter@hotmail.com>
2023-07-31 10:46:30 +0200

    create home- and about-components & import them into App

--- section7/dynamic-components/src/App.vue
+++ section7/dynamic-components/src/App.vue
@@ -1,6 +1,19 @@
-<template></template>
+<template>
+  <select>
+    <option value="Home">Home</option>
+    <option value="About">About</option>
+  </select>
+</template>
+
 <script>
+import Home from "./components/Home.vue";
+import About from "./components/About.vue";
+
 export default {
   name: "App",
+  components: {
+    Home,
+    About,
+  },
 };
 </script>
