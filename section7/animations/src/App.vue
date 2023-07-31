c14eea766e69754dc7cb4e1efae34d208bc008dd
Markus Winter <markusdominicwinter@hotmail.com>
2023-07-31 13:00:15 +0200

    reverse order of the h2 fading-in and fading-out by setting mode-property for the transition-component

--- section7/animations/src/App.vue
+++ section7/animations/src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <button type="button" @click="flag = !flag">Toggle</button>
 
-  <transition name="fade">
-    <h2 v-if="flag">Hello World!</h2>
+  <!-- by default: When animating 2 elements that appear conditionally, Vue animates the second element in & then the first element out -->
+  <!-- ...the property "mode" of the transition-component controls/sets this behaviour -->
+  <transition name="fade" mode="out-in">
+    <!-- v-show / v-if have the same effect here -->
+    <h2 v-if="flag" key="main">Hello World!</h2>
+    <!-- <h2 v-show="flag">Hello World!</h2> -->
+    <h2 v-else key="secondary">Another Hello!</h2>
   </transition>
 </template>
 
@@ -23,12 +28,12 @@ export default {
 }
 
 .fade-enter-active {
-  transition: all 0.25s linear;
+  transition: all 3s linear;
   /* "opacity = 1" does not have to be applied bc its default value is 1 */
 }
 
 .fade-leave-to {
-  transition: all 0.25s linear;
+  transition: all 3s linear;
   opacity: 0;
 }
 </style>
