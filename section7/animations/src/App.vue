051f2e908edabc60511e2f48ecd8661eae18ad7f
Markus Winter <markusdominicwinter@hotmail.com>
2023-08-01 11:09:32 +0200

    comment out animation from chapters71-72 & create new transition-component inside template and apply styling in side style

--- section7/animations/src/App.vue
+++ section7/animations/src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <button type="button" @click="flag = !flag">Toggle</button>
 
+  <!-- Chapter 71-72 -->
   <!-- by default: When animating 2 elements that appear conditionally, Vue animates the second element in & then the first element out -->
   <!-- ...the property "mode" of the transition-component controls/sets this behaviour -->
-  <transition name="fade" mode="out-in">
-    <!-- v-show / v-if have the same effect here -->
-    <h2 v-if="flag" key="main">Hello World!</h2>
-    <!-- <h2 v-show="flag">Hello World!</h2> -->
-    <h2 v-else key="secondary">Another Hello!</h2>
+  <!-- <transition name="fade" mode="out-in"> -->
+  <!-- v-show / v-if have the same effect here -->
+  <!-- <h2 v-if="flag" key="main">Hello World!</h2> -->
+  <!-- <h2 v-show="flag">Hello World!</h2> -->
+  <!-- <h2 v-else key="secondary">Another Hello!</h2> -->
+  <!-- </transition> -->
+
+  <transition name="zoom">
+    <h2 v-if="flag">Hello</h2>
   </transition>
 </template>
 
@@ -23,17 +28,23 @@ export default {
 </script>
 
 <style>
+h2 {
+  width: 400px;
+  margin: 20px;
+  padding: 20px;
+}
+
 .fade-enter-from {
   opacity: 0;
 }
 
 .fade-enter-active {
-  transition: all 3s linear;
+  transition: all 1s linear;
   /* "opacity = 1" does not have to be applied bc its default value is 1 */
 }
 
 .fade-leave-to {
-  transition: all 3s linear;
+  transition: all 1s linear;
   opacity: 0;
 }
 </style>
