064aeeb8cb54fe69d67e369218885e23398c7193
Markus Winter <markusdominicwinter@hotmail.com>
2023-08-02 18:09:28 +0200

    move questions and result-code from App to Components

--- section8/quiz/src/components/Result.vue
+++ section8/quiz/src/components/Result.vue
@@ -1,5 +1,8 @@
 <template>
-  <div></div>
+  <div class="result">
+    <div class="title">You got sample result 1!</div>
+    <div class="desc">Enter a short description here about the result.</div>
+  </div>
 </template>
 
 <script></script>
