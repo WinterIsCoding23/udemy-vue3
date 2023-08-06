6e49cf7298c71d57d6baa698e99ddb88027c4cd7
Markus Winter <markusdominicwinter@hotmail.com>
2023-08-06 13:17:56 +0200

    replace dummy texts for title & description by dynamic variables

--- section8/quiz/src/components/Result.vue
+++ section8/quiz/src/components/Result.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="result">
-    <div class="title">You got sample result 1!</div>
-    <div class="desc">Enter a short description here about the result.</div>
+    <div class="title">{{ result[resultIndex].title }}</div>
+    <div class="desc">{{ result[resultIndex].desc }}</div>
   </div>
 </template>
 
