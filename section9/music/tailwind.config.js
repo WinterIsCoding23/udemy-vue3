598d6dd167dd64e785ab01e15ca93984ae7e0f30
Markus Winter <markusdominicwinter@hotmail.com>
2023-08-08 16:38:34 +0200

    add vue to content-array in module.exports

--- section9/music/tailwind.config.js
+++ section9/music/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   // theme: to modify colors, font-sizes etc.:
   theme: {
     extend: {}
