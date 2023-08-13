14791d94535f1d2cfa6664e346f55f28fdd2fff1
Markus Winter <markusdominicwinter@hotmail.com>
2023-08-13 22:00:06 +0200

    register Form and Field as VeeForm and VeeField vie component-method

--- section9/music/src/includes/validation.js
+++ section9/music/src/includes/validation.js
@@ -1,9 +1,12 @@
-import { Form, Field } from "vee-validate";
+import { Form as VeeForm, Field as VeeField } from "vee-validate";
 
 
 // install(){} can have a second argument "options"
 // ...can be used in the importing component as an object
 // ...in this project not necessary, hence omitted 
 export default (){
-    install(app){}
+    install(app){
+        app.component("VeeForm", VeeForm);
+        app.component("VeeField", VeeField);
+    }
 }
