fd213fd0ce6e30a071a8f9528f961aca1a8232f9
Markus Winter <markusdominicwinter@hotmail.com>
2023-08-13 21:50:59 +0200

    import VeeValidatePlugin into main.js

--- section9/music/src/main.js
+++ section9/music/src/main.js
@@ -7,6 +7,8 @@ import router from './router'
 import '../src/assets/base.css'
 import '../src/assets/main.css'
 
+import VeeValidatePlugin from './includes/validation'
+
 const app = createApp(App)
 
 // all plugins - like Pinia - must be registered (via .use-method) before mounting the app
