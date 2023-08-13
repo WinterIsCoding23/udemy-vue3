57708cd73832850d98890512c02c27ccadd637bd
Markus Winter <markusdominicwinter@hotmail.com>
2023-08-13 21:57:47 +0200

    import Form and Field

--- section9/music/src/includes/validation.js
+++ section9/music/src/includes/validation.js
@@ -1,3 +1,6 @@
+import { Form, Field } from "vee-validate";
+
+
 // install(){} can have a second argument "options"
 // ...can be used in the importing component as an object
 // ...in this project not necessary, hence omitted 
