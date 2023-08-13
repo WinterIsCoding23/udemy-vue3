2edc7c3365d26244170c7855eb51958534bd9a9f
Markus Winter <markusdominicwinter@hotmail.com>
2023-08-13 22:01:14 +0200

    fix error

--- section9/music/src/includes/validation.js
+++ section9/music/src/includes/validation.js
@@ -1,12 +1,11 @@
-import { Form as VeeForm, Field as VeeField } from "vee-validate";
-
+import { Form as VeeForm, Field as VeeField } from 'vee-validate'
 
 // install(){} can have a second argument "options"
 // ...can be used in the importing component as an object
-// ...in this project not necessary, hence omitted 
-export default (){
-    install(app){
-        app.component("VeeForm", VeeForm);
-        app.component("VeeField", VeeField);
-    }
+// ...in this project not necessary, hence omitted
+export default {
+  install(app) {
+    app.component('VeeForm', VeeForm)
+    app.component('VeeField', VeeField)
+  }
 }
