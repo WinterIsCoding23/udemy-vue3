18150fcb3eda166a1219ad074c8c44bbcc91630c
Markus Winter <markusdominicwinter@hotmail.com>
2023-08-13 21:56:15 +0200

    add app as first argument and add comments regarding possible second argument

--- section9/music/src/includes/validation.js
+++ section9/music/src/includes/validation.js
@@ -1,3 +1,6 @@
+// install(){} can have a second argument "options"
+// ...can be used in the importing component as an object
+// ...in this project not necessary, hence omitted 
 export default (){
-    install(){}
+    install(app){}
 }
