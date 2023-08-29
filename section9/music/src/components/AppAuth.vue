77a717cf33ea23eb053af16e7445eed39218600e
Markus Winter <markusdominicwinter@hotmail.com>
2023-08-29 11:42:26 +0200

    fix error-message by deleting '/' for vee-field-tag of password

--- section9/music/src/components/AppAuth.vue
+++ section9/music/src/components/AppAuth.vue
@@ -121,12 +121,17 @@
             <!-- Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Password</label>
-              <vee-field name="password" :bails="false" v-slot="{ field, errors }"/>
-                <input class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded" type="password" placeholder="Password" v-bind="field"/>
+              <vee-field name="password" :bails="false" v-slot="{ field, errors }">
+                <input
+                  class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+                  type="password"
+                  placeholder="Password"
+                  v-bind="field"
+                />
                 <div class="text-red-600" v-for="error in errors" :key="error">
                   {{ error }}
                 </div>
-            </vee-field >              
+              </vee-field>
             </div>
             <!-- Confirm Password -->
             <div class="mb-3">
@@ -191,7 +196,7 @@ export default {
         name: 'required|min:3|max: 100|alpha_spaces',
         email: 'required|min:3|max: 100|email',
         age: 'required|min:2|min_value:18|max_value:120|numeric',
-        password: 'required|min:3|max:100',
+        password: 'required|min:9|max:100|excluded:password',
         confirm_password: 'confirmed:@password',
         country: 'required|excluded:Antarctica,Germany',
         tos: 'required'
