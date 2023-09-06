e08f40dd02e506ab1a9d16255414b314c47602b7
Markus Winter <markusdominicwinter@hotmail.com>
2023-09-06 17:21:22 +0200

    copy and paste firebaseConfig-object from firebase

---
+++ section9/music/src/includes/firebase.js
@@ -0,0 +1,13 @@
+// firebase/app = core-module --> has ALWAYS to be imported to be able to use other features!
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD1kW13UhXN-j3Nxs5nUgWSdkVRdURU_kg',
+  authDomain: 'music-7e137.firebaseapp.com',
+  projectId: 'music-7e137',
+  storageBucket: 'music-7e137.appspot.com',
+  messagingSenderId: '806297896009',
+  appId: '1:806297896009:web:652b6b2588268cd8dbeb98'
+}
+
+export default firebase.initializeApp(firebaseConfig)
