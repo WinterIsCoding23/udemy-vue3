ee3e85c849873c9e9187ae5687a707da91baada4
Markus Winter <markusdominicwinter@hotmail.com>
2023-09-06 17:46:31 +0200

    add comments in vite.config.js & make register-function in RegisterForm.vue async and chain firebase-method createUserWithEmailAndPassword to firebase.auth

--- section9/music/src/components/RegisterForm.vue
+++ section9/music/src/components/RegisterForm.vue
@@ -105,6 +105,8 @@
 </template>
 
 <script>
+import firebase from '../includes/firebase'
+
 export default {
   name: 'RegisterForm',
   data() {
@@ -133,12 +135,14 @@ export default {
     }
   },
   methods: {
-    register(values) {
+    async register(values) {
       // console.log(values)
       this.reg_show_alert = true
       this.reg_in_submission = true
       this.reg_alert_variant = 'bg-blue-500'
       this.reg_alert_msg = 'Please wait! Your account is being created.'
+
+      await firebase.auth().createUserWithEmailAndPassword()
 
       this.reg_alert_variant = 'bg-green-500'
       this.reg_alert_msg = 'Success! Your account has been created.'
