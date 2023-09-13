b432ab83495a54ad95698539c7da3b62f2885786
Markus Winter <markusdominicwinter@hotmail.com>
2023-09-13 18:12:08 +0200

    add ...mapWritableState to computed-property & set this.userLoggedIn to true after authentication

--- section9/music/src/components/RegisterForm.vue
+++ section9/music/src/components/RegisterForm.vue
@@ -136,6 +136,9 @@ export default {
       reg_alert_msg: 'Please wait - your account is bein created.'
     }
   },
+  computed: {
+    ...mapWritableState(useUserStore, ['userLoggedIn'])
+  },
   methods: {
     async register(values) {
       // console.log(values)
@@ -172,6 +175,8 @@ export default {
         this.reg_alert_msg = 'An unexpected error occurred. Please try again later.'
         return
       }
+
+      this.userLoggedIn = true
 
       this.reg_alert_variant = 'bg-green-500'
       this.reg_alert_msg = 'Success! Your account has been created.'
